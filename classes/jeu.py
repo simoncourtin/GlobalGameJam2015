72fc6bc9cfcb45a8739c0cb2e4c7194114d4e3aa
ThibaultTricard <thibault.tricard@hotmail.fr>
2015-01-24 05:02:25 +0100

    coucou

--- classes/jeu.py
+++ classes/jeu.py
@@ -22,11 +22,6 @@ class Jeu():
         self.joueurs.add(player.Player(1))
         self.joueurs.add(player.Player(2))
         self.joueurs.add(player.Player(3))
-
-        groupe_sansJ = pygame.sprite.Group()
-        for j in self.joueurs:
-            if not j is self.playerById(self.id_client):
-                groupe_sansJ.add(j)
         
         #definition du sprite controlable
         self.playerById(self.id_client).setControllable(True)
@@ -50,24 +45,16 @@ class Jeu():
                     self.socket.close()
                     return
             
-
             
-            collision=pygame.sprite.spritecollide(self.playerById(self.id_client),groupe_sansJ,False)
-            
-            for other in collision:
-                self.playerById(self.id_client).life -= 10
-                print 'hit'
-                if (self.playerById(self.id_client).life <= 0):
-                    print "You dead"
-            """
+            collision=pygame.sprite.spritecollide(self.playerById(self.id_client),self.joueurs,False)
             if len(collision) > 1:
                 if(colliding==1):
-                    print collision
+                    print colliding
                     if(len(collision) == 1):
                         colliding=0
-                        print collision
+                        print colliding
                 else:
-                    print collision
+                    print colliding
                     if(len(collision) > 1):
                         colliding=1
                         print 'encule !'+str(self.id_client)
@@ -75,7 +62,6 @@ class Jeu():
                         if(self.playerById(self.id_client).life==0):
                             print("perdu")
                             return
-            """
     
             self.screen.fill((0,0,0))
             self.joueurs.update()
