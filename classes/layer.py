ac3edf756cf6bc17b2356ef2dacdb7b29b94e503
simoncourtin <courtin.simon@gmail.com>
2015-01-24 06:12:57 +0100

    Tilset editrioshjsrohslhslgf

--- classes/layer.py
+++ classes/layer.py
@@ -1,6 +1,40 @@
 __author__ = 'Simon'
+import pygame
+import math
 class Layer():
 
-    def __init__(self):
+    def __init__(self,screen,fichier,x_tile=32,y_tile=32):
+        #fenetre ou ajouter
+        self.screen = screen
+        #image du calque
+        self.mur = pygame.image.load('../maps/cobblestone/mur.png').convert_alpha()
+        self.plant = pygame.image.load('../maps/cobblestone/grass.png').convert_alpha()
+        self.tilset = pygame.image.load('../maps/cobblestone/cobblestone.png').convert_alpha()
+        #lecture du fichier
+        self.f = open(fichier, "r")
+        self.ligne = self.f.read()
+        self.ligne = self.ligne.replace('\n','\n,')
+        #generation du tableau pour les differents element du calque
         self.calque=[]
-        
+        self.calque= self.ligne.split(',')
+        #hauteur et largeur des tuile
+        self.x_tile=x_tile
+        self.y_tile=y_tile
+
+
+    def afficher_layer(self):
+        hauteur = self.x_tile
+        largeur = self.y_tile
+        x=0
+        y=0
+        for element in self.calque:
+            if element != '\n':
+                if element!='0':
+                    X = (((int(element))-(((int(element)-1)/16)*16))-1)*32
+                    Y=(int(element)/16)*32
+                    print element,X, Y
+                    self.screen.blit(self.tilset.subsurface(X,Y,hauteur,largeur),(x,y))
+                x+=largeur
+            else:
+                y+=hauteur
+                x=0
--- classes/map.py
+++ classes/map.py
@@ -1,32 +1,31 @@
 __author__ = 'Simon'
 import  pygame
+import layer
+from pygame.locals import *
+import sys
 class Map():
 
     def __init__(self,screen):
         self.calques = []
         self.screen =screen
-
-
-    def afficher_calque(self,calque):
-        for element in calque:
-            if element > -1:
-                print element
-            else:
-                print element
+        self.calques.append(layer.Layer(self.screen,"../maps/cobblestone/collision.txt",30,30))
 
     def afficher_map(self):
         for calque in self.calques:
-            self.afficher_calque(calque)
+            calque.afficher_layer()
 
 
 
 
 
 pygame.init()
-screen = pygame.display.set_mode((800,800))
+screen = pygame.display.set_mode((1920,1080))
 pygame.display.set_caption('Play game')
 map = Map(screen)
+map.afficher_map()
 pygame.display.flip()
 
 while True:
-    print "ca tourne"
+    for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit(0)
