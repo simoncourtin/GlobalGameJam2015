cd12a928ae6faed7597829d0588d31b1bee50669
Rodnor <rodnor@live.fr>
2015-01-24 12:06:50 +0100

    De la merde

---
+++ classes/item.py
@@ -0,0 +1,22 @@
+__author__ = 'Simon'
+import pygame
+from pygame.locals import *
+
+class Item(pygame.sprite.Sprite):
+
+    BASE_RESSOURCE = "images/"
+
+    def __init__(self,  jeu, resource, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.jeu = jeu
+        self.resource = BASE_RESSOURCE + resource
+        self.rect.x=x
+        self.rect.y=y
+        self.image=pygame.image.load(self.resource)
+        self.screen = self.jeu.screen
+        
+    def afficherItem(self, jeu):
+        self.screen.blit(self.image, (self.rect.x,self.rect.y))
+    
+    def pickUpItem(self, jeu):
+        
