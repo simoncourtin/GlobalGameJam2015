0bd2c773f9d88b9d6e373e30059663c6aea622ae
simoncourtin <courtin.simon@gmail.com>
2015-01-24 06:36:35 +0100

    jhgnf

--- classes/map.py
+++ classes/map.py
@@ -8,8 +8,8 @@ class Map():
     def __init__(self,screen):
         self.calques = []
         self.screen =screen
-        self.calques.append(layer.Layer(self.screen,"../maps/cobblestone/background.txt",'../maps/cobblestone/cobblestone.png',30,30))
-        self.calques.append(layer.Layer(self.screen,"../maps/cobblestone/collision.txt",'../maps/cobblestone/cobblestone.png',30,30))
+        self.calques.append(layer.Layer(self.screen,"maps/cobblestone/background.txt",'maps/cobblestone/cobblestone.png',30,30))
+        self.calques.append(layer.Layer(self.screen,"maps/cobblestone/collision.txt",'maps/cobblestone/cobblestone.png',30,30))
 
     def afficher_map(self):
         for calque in self.calques:
