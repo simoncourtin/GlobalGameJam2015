ab4c1ca10132a7fa658e3eafde9dd6fae7f2aabb
simoncourtin <courtin.simon@gmail.com>
2015-01-25 13:35:07 +0100

    les objets dans les layer

--- classes/map.py
+++ classes/map.py
@@ -7,21 +7,28 @@ class Map():
 
     def __init__(self,screen):
         self.spawn = []
+        self.objets = []
         self.calques = []
         self.screen =screen
-        self.layer1 = layer.Layer(self.screen,"maps/cobblestone2/background.txt",'maps/cobblestone2/cobblestone.png',False,30,30)
+        self.layer1 = layer.Layer(self.screen,"maps/cobblestone2/background.txt",'maps/cobblestone2/cobblestone.png',False,False,30,30)
         self.calques.append(self.layer1)
-        self.layer2 = layer.Layer(self.screen,"maps/cobblestone2/collision.txt",'maps/cobblestone2/cobblestone.png',True,30,30)
+        self.layer2 = layer.Layer(self.screen,"maps/cobblestone2/collision.txt",'maps/cobblestone2/cobblestone.png',True,False,30,30)
         self.calques.append(self.layer2)
-        self.layer3 = layer.Layer(self.screen,"maps/cobblestone2/spawn.txt",'maps/cobblestone2/cobblestone.png',False,30,30)
+        self.layer3 = layer.Layer(self.screen,"maps/cobblestone2/spawn.txt",'maps/cobblestone2/cobblestone.png',False,False,30,30)
         self.calques.append(self.layer3)
         self.spawn=self.layer3.spawn
+        self.layer4 = layer.Layer(self.screen,"maps/cobblestone2/piece.txt",'maps/cobblestone2/piece_tile.png',False,True,30,30)
+        self.calques.append(self.layer3)
+        self.objets=self.layer4.objets
     def afficher_map(self, camera):
         for calque in self.calques:
             calque.afficher_layer(camera)
 
     def getSpawn(self):
         return self.spawn
+    def getObjets(self):
+        return self.objets
+
 
 
 
