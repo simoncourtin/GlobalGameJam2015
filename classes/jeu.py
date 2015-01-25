0333b4620beafb4b5ef740653c7d0a568fef3fc5
ThibaultTricard <thibault.tricard@hotmail.fr>
2015-01-25 11:18:34 +0100

    serveur

--- classes/jeu.py
+++ classes/jeu.py
@@ -117,6 +117,8 @@ class Jeu():
             for event in pygame.event.get():
                 if event.type == QUIT:
                     self.socket.send("QUIT")
+                    self.producer.stop()
+                    self.consumer.stop()
                     reponse = self.socket.recv(1024)
                     self.socket.close()
 
