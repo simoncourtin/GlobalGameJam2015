0ead1e6302b8ca12da9aa05cce174e2db0cb82cd
Simon Garrelou <simon@localhost.localdomain>
2015-01-25 14:43:41 +0100

    Revert "nouveau fond sonore"
    This reverts commit 1bb3ffb919a2806d3a0da82b2e587e6d09af642d.
    
    Conflicts:
    	classes/jeu.py

--- classes/jeu.py
+++ classes/jeu.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import pygame
 import time
 from pygame import sprite
@@ -13,7 +16,7 @@ NB_PIECES = 3
 MAX_FPS = 60
 FONT_SIZE = 16
 FONT_STYLE = None
-KEY_REPEAT_DELAY = 2000  # milliseconds
+KEY_REPEAT_DELAY = 1000  # milliseconds
 
 WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 700
@@ -35,7 +38,9 @@ BLEU = (0, 0, 255)
 class Jeu():
     def __init__(self, nom_joueur, socket):
         pygame.init()
-        
+
+        self.idnom = idnom
+
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Broken pipe')
         self.font = pygame.font.Font(FONT_STYLE, FONT_SIZE)
@@ -46,7 +51,7 @@ class Jeu():
         self.map = map.Map(self.screen)
 
         self.distribution_spawn(self.map)
-        
+
         self.camps = pygame.sprite.Group()
         self.camp_rouge = camp.Camp(0, BASE_ROUGE_X, BASE_ROUGE_Y, "Camp Rouge", "rouge")
         self.camp_bleu = camp.Camp(1, BASE_BLEUE_X, BASE_BLEUE_Y, "Camp Bleu", "bleu")
@@ -65,13 +70,13 @@ class Jeu():
             else:
                 self.joueurs.add(player.Player(self, self.camp_bleu, i, self.idnom[i], BASE_BLEUE_X, BASE_BLEUE_Y))
 
-
         self.current_player = self.playerById(self.id_client)
 
         self.groupe_attaque = pygame.sprite.Group()
 
         self.items = pygame.sprite.Group()
         self.items_taken = pygame.sprite.Group()
+<<<<<<< HEAD
 
         i = 0
         for obj in self.map.objets:
@@ -95,8 +100,8 @@ class Jeu():
 
         # load de toutes les musiques + bruitages
         pygame.mixer.music.load("fondSonore.ogg")
-        stress = pygame.mixer.Sound("stress.ogg")
-        #pygame.mixer.music.queue("fondSonore.ogg")
+        # self.stress = pygame.mixer.Sound("stress.ogg")
+        # pygame.mixer.music.queue("fondSonore.ogg")
         pickCoins = pygame.mixer.Sound("pickCoins.ogg")
         missCoins = pygame.mixer.Sound("missCoins.ogg")
         select = pygame.mixer.Sound("select.ogg")
@@ -117,7 +122,7 @@ class Jeu():
 
         clock = pygame.time.Clock()
         timeLastAttack = pygame.time.get_ticks()
-        
+
         # LOOP
         while True:
             clock.tick(MAX_FPS)
@@ -133,15 +138,22 @@ class Jeu():
                         self.current_player.setSpeed(0)
 
                         if timeLastAttack + KEY_REPEAT_DELAY < pygame.time.get_ticks():
-                            target = pygame.sprite.spritecollide(self.current_player, self.joueurs, False)
+                            self.current_player.updateAttaque()
+                            self.groupe_attaque.add(self.current_player.attaque)
+
+                            group_sans_J = [j for j in self.joueurs if j.classe != self.id_client]
+
+                            # On test le collide du joueur et de l'epee
+                            target = pygame.sprite.spritecollide(self.current_player.attaque, group_sans_J, False)
                             self.current_player.attack(target)
+
                             timeLastAttack = pygame.time.get_ticks()
 
                     if event.key == K_e:
                         coins = pygame.sprite.spritecollide(self.current_player, self.items, False)
                         # On ne garde que les pieces n'appartenant pas au camp du joueur
                         coins = [it for it in coins if it.camp != self.current_player.camp]
-                        
+
                         if coins:
                             if self.current_player.pickUpItem(coins):
                                 pickCoins.play()
@@ -153,65 +165,55 @@ class Jeu():
                 elif event.type == KEYUP:
                     if event.key == K_SPACE:
                         self.current_player.setSpeed(player.VELOCITY)
+                        self.groupe_attaque.remove(self.current_player.attaque)
 
 
             # Verification de la victoire
             if len(self.camp_rouge.pieces_depart) <= 0:
                 if self.current_player.camp.nom == "Camp Rouge":
                     pygame.mixer.music.stop()
-                    stress.stop()
                     defaite.play()
                 else:
                     pygame.mixer.music.stop()
-                    stress.stop()
                     victoire.play()
-                
+
                 print "LES BLEUS ONT GAGNE, BRAVO !!"
                 break
-            
+
             elif len(self.camp_bleu.pieces_depart) <= 0:
                 if self.current_player.camp.nom == "Camp Bleu":
                     pygame.mixer.music.stop()
-                    stress.stop()
                     defaite.play()
                 else:
                     pygame.mixer.music.stop()
-                    stress.stop()
                     victoire.play()
 
                 print "LES ROUGES ONT GAGNE, BRAVO !!"
                 break
-                        
+
             collision_camp = pygame.sprite.collide_rect(self.current_player, self.current_player.camp)
             if collision_camp and len(self.current_player.items) > 0:
                 self.current_player.deposerItem()
-                        
+
             self.joueurs.update()
-            
+            self.groupe_attaque.update()
+
             # Gestion de la camera
-            self.cam.update(self.current_player) # Centre sur le joueur
+            self.cam.update(self.current_player)  # Centre sur le joueur
 
             # rafraichissement de la map des des affichages des joueurs
-            self.map.afficher_map(self.cam)          
+            self.map.afficher_map(self.cam)
 
-            
-            # Affichage du sprite d'attaque
-            if self.current_player.attaque.getVisible() :
-                self.groupe_attaque.add(self.current_player.attaque)
-                self.groupe_attaque.clear(self.screen,self.current_player.attaque.image)
+            if self.groupe_attaque:
+                self.groupe_attaque.clear(self.screen, self.current_player.attaque.image)
                 self.groupe_attaque.draw(self.current_player.attaque.image)
-                self.screen.blit(self.current_player.attaque.image, self.cam.apply(self.current_player.attaque))
-
-            # Cacher le sprite d'attaque
-            if not self.current_player.attaque.getVisible() :
-                self.groupe_attaque.remove(self.current_player.attaque)
-
-                
+                self.screen.blit(self.current_player.attaque.image,
+                                 self.cam.apply(self.current_player.attaque))
 
             # On blit les camps
             for c in self.camps:
                 self.screen.blit(c.image, self.cam.apply(c))
-                
+
             # On blit les joueurs
             for j in self.joueurs:
                 self.screen.blit(j.image, self.cam.apply(j))
@@ -235,7 +237,6 @@ class Jeu():
                     text, rect = joueur.getHealthbar().displayLife(joueur.getX() - 20, joueur.getY() - 10)
                     self.screen.blit(text, self.cam.apply_rect(rect))
 
-            
             pygame.display.flip()
 
             # pygame.draw.line(self.screen, (180, 0, 0), (SCOREBOARD_TOP_X, SCOREBOARD_TOP_Y), (WINDOW_WIDTH * 2 / 8, SCOREBOARD_TOP_Y), 50)
@@ -247,16 +248,19 @@ class Jeu():
             if j.classe == id_player:
                 return j
 
+
     def itemById(self, id_item):
         for i in self.items:
             if i.id_item == id_item:
                 return i
 
+
     def campById(self, id_camp):
         for c in self.camps:
             if c.id_camp == id_camp:
                 return c
-            
+
+
     # Affiche le scoreboard
     def displayScore(self, joueur, xAbs, yAbs):
         handlebar = joueur.getHealthbar()
@@ -302,4 +306,3 @@ class Jeu():
             resultat = recup_message(self.socket)
             donnee = resultat.split(' ')
         self.idnom[int(donnee[1])] = donnee[2]
-
