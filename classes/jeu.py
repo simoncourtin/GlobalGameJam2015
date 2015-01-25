ae4f6ed353de5cff8e140334bcf0e9872e1baf75
Simon Garrelou <simon@localhost.localdomain>
2015-01-25 12:08:58 +0100

    Fix de la musique du stress

--- classes/jeu.py
+++ classes/jeu.py
@@ -148,14 +148,6 @@ class Jeu():
                     if event.key == K_SPACE:
                         self.current_player.setSpeed(player.VELOCITY)
 
-            # Declenchement de la musique du stress
-            if len(self.camp_rouge.pieces_depart) <= 2:
-                pygame.mixer.music.stop()
-                stress.play()
-
-            if len(self.camp_bleu.pieces_depart) <= 2:
-                pygame.mixer.music.stop()
-                stress.play()
 
             # Verification de la victoire
             if len(self.camp_rouge.pieces_depart) <= 0:
