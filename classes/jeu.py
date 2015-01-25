04dc4e02bcc4403d4776d346c07ca7370381ef75
Simon Garrelou <simon@localhost.localdomain>
2015-01-25 13:24:58 +0100

    Fix du stress

--- classes/jeu.py
+++ classes/jeu.py
@@ -161,11 +161,9 @@ class Jeu():
             if len(self.camp_rouge.pieces_depart) <= 0:
                 if self.current_player.camp.nom == "Camp Rouge":
                     pygame.mixer.music.stop()
-                    self.stress.stop()
                     defaite.play()
                 else:
                     pygame.mixer.music.stop()
-                    self.stress.stop()
                     victoire.play()
 
                 print "LES BLEUS ONT GAGNE, BRAVO !!"
@@ -174,11 +172,9 @@ class Jeu():
             elif len(self.camp_bleu.pieces_depart) <= 0:
                 if self.current_player.camp.nom == "Camp Bleu":
                     pygame.mixer.music.stop()
-                    self.stress.stop()
                     defaite.play()
                 else:
                     pygame.mixer.music.stop()
-                    self.stress.stop()
                     victoire.play()
 
                 print "LES ROUGES ONT GAGNE, BRAVO !!"
